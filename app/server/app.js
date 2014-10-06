4931a6f56a154fd024eb137cf33e7c10b024b875
justin.fiedler <justin.fiedler@hightail.com>
2014-10-06 11:44:12 -0700

    clinet.app.connectionFilters are now on the request (rather than the global server wilsonConfig Added wilson-locals middleware to add res.locals.wilsonConfig to make it esaier to use the config in templates Some code refactoring to prevent compiling HBS tempaltes multiple times

--- app/server/app.js
+++ app/server/app.js
@@ -21,23 +21,22 @@ var wilson = Wilson(app, wilsonConfigJson);
 //attach the wilson routes under '/wilson'
 app.use('/wilson', wilson.router);
 
-
-
 //Load index page
 var indexHbsPath = path.join(wilson.config.server.projectPaths.root, '/server/templates/index.hbs');
 var indexHbs = fs.readFileSync(indexHbsPath, 'utf8');
+var indexTemplate = hbs.compile(indexHbs);
 
-
+// CATCH ALL ROUTE
 app.get('*', wilson.middleware, function(req, res) {
-//  console.log('render template');
-  var template = hbs.compile(indexHbs);
-  var result = template({
-    wilsonConfig: _.merge({}, req.wilson, wilson.config)
-  });
+  //console.log('render template');
+
+  //TODO: Is there a way to not have to pass in res.locals? Maybe res.render() instead of send
+  var result = indexTemplate(res.locals);
 
   res.send(result);
 });
 
+// Start the server
 http.createServer(app).listen(app.get('port'), function() {
   console.log('Express server listening on http port ' + app.get('port'));
 });
