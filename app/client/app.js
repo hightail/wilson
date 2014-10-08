622ad99d986fd37b6d70470c8e30cbc7a78e7449
justin.fiedler <justin.fiedler@hightail.com>
2014-10-08 16:01:08 -0700

    Removed demo app Updated license to MIT Added client.wilson.js files to git

--- app/client/app.js
+++
@@ -1,9 +0,0 @@
-angular.module('myApp', ['wilson']).
-  config(function() {
-  }).
-  run(['$rootScope', '$location', 'AppService',
-    function($rootScope, $location, AppService) {
-      console.log('app.run()');
-      AppService.log('Testing');
-    }
-  ]);
--- app/server/tag-middleware/brand.js
+++
@@ -1,6 +0,0 @@
-module.exports = function(req, res, next) {
-  req.wilson.tags.brand = 'hightail';
-  //console.log('Set brand', req.wilson.tags.brand);
-
-  next();
-};
