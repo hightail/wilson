b7f1bcb6d5774d560f1becc1489f8827db49b487
Hunter Novak <hunternovak@gmail.com>
2016-09-27 21:46:59 -0700

    Moved from grunt to gulp.

---
+++ gulpfile.js
@@ -0,0 +1,28 @@
+/**
+ * Gulpfile to build and test Wilson
+ */
+var gulp    = require('gulp'),
+    concat  = require('gulp-concat'),
+    rename  = require('gulp-rename'),
+    uglify  = require('gulp-uglify');
+
+
+gulp.task('build', function() {
+  return gulp.src(['lib/client/src/wilson.js', 'lib/client/src/**/*.js'])
+    .pipe(concat('client.wilson.js'))
+    .pipe(gulp.dest('lib/client'))
+    .pipe(uglify({
+      output: {
+        'preamble':   '/* Wilson Client Framework ' + (new Date().toUTCString()) + ' */\n'
+      }
+    }))
+    .pipe(rename('client.wilson.min.js'))
+    .pipe(gulp.dest('lib/client'));
+});
+
+gulp.task('default', ['build']);
+
+
+
+
+
