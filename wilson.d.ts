7a48df29c82eb665d981a69368c66ffa1a998b13
Hunter Novak <hunternovak@gmail.com>
2017-04-19 22:22:37 -0700

    Added built-in wilson logger.  Updated wilson typescript definition. Removed debugMode support that is no longer useful or necessary.  Removed redundant utils declaration that was confusing.

--- wilson.d.ts
+++ wilson.d.ts
@@ -43,6 +43,20 @@ interface WilsonUtils {
 }
 
 
+interface WilsonLogger {
+
+  setLevel(logLevel: string): void;
+
+  // Logging Methods
+  trace(message: string): void;
+  debug(message: string): void;
+  info(message: string): void;
+  warn(message: string): void;
+  error(message: string): void;
+  fatal(message: string): void;
+}
+
+
 interface Wilson {
 
   // Properties
